c1c8996f967646fc4f7e0eb5b751281e7e8bcdbf
FajarGD <146056446+desrian19-ai@users.noreply.github.com>
2026-09-13 19:04:31 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,41 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/admin.html',
+        destination: '/admin',
+        permanent: true,
+      },
+      {
+        source: '/index.html',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/daftar.html',
+        destination: '/daftar',
+        permanent: true,
+      },
+      {
+        source: '/tiket.html',
+        destination: '/tiket',
+        permanent: true,
+      },
+      {
+        source: '/antrian.html',
+        destination: '/admin',
+        permanent: true,
+      },
+      {
+        source: '/berita.html',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
